9a6b74da103c9ac82df62d5ad289497f185b1901
shirinkhanse <41347510+shirinkhanse@users.noreply.github.com>
2025-02-27 19:35:31 -0600

    Update dnac_config.py

--- dnac_config.py
+++ dnac_config.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 """
 
-Copyright (c) 2021 Cisco and/or its affiliates.
+Copyright (c) 2025 Cisco and/or its affiliates.
 
 This software is licensed to you under the terms of the Cisco Sample
 Code License, Version 1.1 (the "License"). You may obtain a copy of the
@@ -20,15 +18,14 @@ or implied.
 """
 
 __author__ = "Shirin Khan <shirkhan@cisco.com>"
-__copyright__ = "Copyright (c) {{current_year}} Cisco and/or its affiliates."
+__copyright__ = "Copyright (c) 2025 Cisco and/or its affiliates."
 __license__ = "Cisco Sample Code License, Version 1.1"
 
 
 # This file contains the:
-# Cisco DNA Center username and password, server info
+# Cisco Catalyst Center username and password, server info
 
 # Update this section with the DNA Center server info and user information
-#DNAC_URL_REGION1 = 'https://10.122.21.78'
 DNAC_URL_REGION1 = 'https://<ip address>'
 DNAC_USER_REGION1 = b'<copy and paste encrypted binary value here from ENC_USER1_FILE, please keep the single quote and b>'
 DNAC_PASS_REGION1 = b'<copy and paste encrypted binary value here from ENC_PASS1_FILE, please keep the single quote and b>'
